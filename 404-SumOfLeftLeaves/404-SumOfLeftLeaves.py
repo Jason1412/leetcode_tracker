74b9cfbb4936d866c57cd118c217e1c754da1a80
Jason1412 <jasonliu1412@gmail.com>
2026-05-16 12:28:09 -0700

    Create 404-SumOfLeftLeaves

---
+++ 404-SumOfLeftLeaves/404-SumOfLeftLeaves.py
@@ -0,0 +1,27 @@
+# Last updated: 5/16/2026, 12:28:09 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+
+        self.dfs(root)
+
+        return self.res
+
+
+    def dfs(self, root):
+
+        if root is None:
+            return 
+
+        if root.left and not root.left.left and not root.left.right:
+            self.res += root.left.val
+
+
+        self.dfs(root.left)
+        self.dfs(root.right) 
