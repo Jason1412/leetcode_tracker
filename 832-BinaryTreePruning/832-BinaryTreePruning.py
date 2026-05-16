b624c4cf9a8f8d5ad58e82bb57704723d2c1bfd3
Jason1412 <jasonliu1412@gmail.com>
2026-05-16 12:26:43 -0700

    Create 832-BinaryTreePruning

---
+++ 832-BinaryTreePruning/832-BinaryTreePruning.py
@@ -0,0 +1,20 @@
+# Last updated: 5/16/2026, 12:26:43 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        if root is None:
+            return 
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+
+        if root.val == 0 and root.left is None and root.right is None:
+            return None
+        
+        return root
