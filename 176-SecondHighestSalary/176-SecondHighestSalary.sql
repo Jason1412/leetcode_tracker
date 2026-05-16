ce697193ae86fc9b09cfac9114cc8ef7f39aca16
Jason1412 <jasonliu1412@gmail.com>
2026-05-16 12:29:14 -0700

    Create 176-SecondHighestSalary

---
+++ 176-SecondHighestSalary/176-SecondHighestSalary.sql
@@ -0,0 +1,5 @@
+-- Last updated: 5/16/2026, 12:29:14 PM
+# Write your MySQL query statement below
+SELECT max(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee);
