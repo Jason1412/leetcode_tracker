0fa1d3a3365e74dd3ffbdddd23bb114e4fae3590
Jason1412 <jasonliu1412@gmail.com>
2026-05-16 12:28:17 -0700

    Create 371-SumOfTwoIntegers

---
+++ 371-SumOfTwoIntegers/371-SumOfTwoIntegers.py
@@ -0,0 +1,9 @@
+# Last updated: 5/16/2026, 12:28:17 PM
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return sum([a,b])
