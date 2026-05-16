93777d2f8aa0283ee164bb8d0695d69330447c73
Jason1412 <jasonliu1412@gmail.com>
2026-05-16 12:29:03 -0700

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,5 @@
+-- Last updated: 5/16/2026, 12:29:03 PM
+# Write your MySQL query statement below
+SELECT E1.Id 
+FROM Weather AS E1, Weather AS E2
+WHERE E1.Temperature > E2.Temperature AND DATEDIFF(E1.Date, E2.Date) = 1
