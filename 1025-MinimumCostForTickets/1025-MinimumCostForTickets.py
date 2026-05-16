12eafebd27ef24ef743746c2ea4240eb6d68a10d
Jason1412 <jasonliu1412@gmail.com>
2026-05-16 12:26:09 -0700

    Create 1025-MinimumCostForTickets

---
+++ 1025-MinimumCostForTickets/1025-MinimumCostForTickets.py
@@ -0,0 +1,17 @@
+# Last updated: 5/16/2026, 12:26:09 PM
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+
+        days.sort(reverse= True)
+        maxDay = days[0]
+        passDays = [1, 7, 30]
+        f = [0] + [1000000] * maxDay 
+        
+        for j in range(1, maxDay + 1): 
+            if j not in days: 
+                f[j] = f[j - 1]
+            else: 
+                f[j] = min(f[max(j - 1, 0)] + costs[0], f[max(j - 7, 0)] + costs[1], f[max(j - 30, 0)] + costs[2])
+        
+        
+        return f[maxDay]
