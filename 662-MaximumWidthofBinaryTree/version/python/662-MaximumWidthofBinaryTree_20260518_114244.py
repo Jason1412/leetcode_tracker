f7792ab3d3aba846c24493f6fb424c2202504dc4
Jason1412 <jasonliu1412@gmail.com>
2026-05-18 11:42:44 -0700

    Create 662-MaximumWidthofBinaryTree

---
+++ 662-MaximumWidthofBinaryTree/version/python/662-MaximumWidthofBinaryTree_20260518_114244.py
@@ -0,0 +1,54 @@
+# Last updated: 5/18/2026, 11:42:44 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7
+8from collections import deque
+9class Solution:
+10    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+11        
+12        if not root:
+13            return 0
+14
+15        q = deque([(root, 1)])
+16
+17        maxWidth = 0
+18
+19
+20
+21        while q:
+22            sz = len(q)
+23
+24
+25            minLevel = float('inf')
+26            maxLevel = float('-inf')
+27
+28            for i in range(sz):
+29                node, index = q.popleft()
+30                minLevel = min(index, minLevel)
+31                maxLevel = max(index, maxLevel)
+32
+33                # if i == 0:
+34                #     start = index
+35
+36                # if i == sz - 1:
+37                #     end = index
+38                # print("minLevel=", minLevel)
+39                # print("maxLevel=", maxLevel)
+40                # print("----------")
+41
+42                if node.left:
+43                    q.append((node.left, index * 2))
+44                
+45                if node.right:
+46                    q.append((node.right, index * 2 + 1))
+47
+48        
+49            # maxWidth = max(end - start + 1, maxWidth)
+50            maxWidth = max(maxLevel - minLevel + 1, maxWidth)
+51
+52
+53        return maxWidth
